4c4b89e19763f52269672e7ff258f6a54eb3f790
Jeff-Adler <Jeffm.adler@gmail.com>
2020-08-08 20:28:42 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const limitString = `?_limit=${limit}`
         const pageString = `&_page=${page}`
         const fetchUrl = url + limitString + pageString
-        console.dir(fetchUrl)
         fetch(fetchUrl)
         .then(response => response.json())
         .then(function(monsters) {
@@ -59,10 +58,24 @@ document.addEventListener("DOMContentLoaded", () => {
         h4.innerText = `Age: ${monster.age}`
         const p = document.createElement("p")
         p.innerText = `Bio: ${monster.description}`
+        const button = document.createElement("button")
+        button.id = "edit-button"
+        button.innerHTML = `Edit`
         div.appendChild(h2)
         div.appendChild(h4)
         div.appendChild(p)
+        div.appendChild(button)
         document.querySelector("#monster-container").append(div)
+    }
+
+    const loadEditForm = () => {
+        const form = document.createElement("form")
+        form.className = "edit-form"
+        form.innerHTML =   `<input id="name" placeholder="name...">
+                            <input id="age" placeholder="age...">
+                            <input id="description" placeholder="description...">
+                            <button>Create</button>`
+        return form
     }
 
     const clickHandler = () => {
@@ -78,14 +91,23 @@ document.addEventListener("DOMContentLoaded", () => {
                     page -= 1
                     getMonsters()
                 } 
+            } else if (click.matches("#edit-button")) {
+                const div = click.parentNode
+                click.style.display = "none"
+                div.appendChild(loadEditForm())
             }
         })
     }
 
     const submitHandler = () => {
-        const form = document.querySelector("#monster-form")
-        form.addEventListener("submit", (e) => {
-            createMonster(e.target)
+        document.addEventListener("submit", (e) => {
+            if (e.target.matches("#monster-form")) {
+                createMonster(e.target)
+            }
+            else if (e.target.matches(".edit-form")) {
+                e.preventDefault
+                editMonster(e.target)
+            }
         })
     }
 
@@ -111,6 +133,43 @@ document.addEventListener("DOMContentLoaded", () => {
         } 
 
         fetch(url, options) 
+    }
+
+    const editMonster = (Monster) => {
+
+        const name = Monster.name.value
+        const age = Monster.age.value
+        const description = Monster.description.value
+        const id = Monster.parentNode.id
+  
+        const monsterObj = { 
+          name: name,
+          age: age, 
+          description: description, 
+        }
+
+        const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify(monsterObj)
+        } 
+
+        console.log(url + id)
+        fetch(url + id, options) 
+        .then (response => response.json)
+        .then(function(monster) {
+            updateMonsterView(monster)
+        })
+        .catch(function(error) {
+            console.log(error.message)
+        })
+    }
+
+    const updateMonsterView = (monster) => {
+        console.dir(monster)
     }
 
     loadForm()
