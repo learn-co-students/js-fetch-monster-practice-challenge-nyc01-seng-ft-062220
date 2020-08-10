2e4cbdce43d1337543ddf435933ac830b030029f
github username <github email address>
2020-08-09 22:57:06 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,76 +1,108 @@
 document.addEventListener("DOMContentLoaded", function(e){
+  
+  
+  let base_link = `http://localhost:3000/monsters/?_limit=50&_page=1`
+  
+  fetch(base_link)
+  .then(resp =>resp.json())
+  .then(monsters => monsters.forEach(monster =>{
+    renderMonsters(monster)
+  })
+  )
+  
+  function renderMonsters(monster){
+    let monsterCont = document.querySelector("#monster-container")
+    let monsterDiv = document.createElement('div')
+    monsterDiv.innerHTML=`
+    <h2>${monster.name}<h2/>
+    <h4>${monster.age}<h4/>
+    <p>${monster.description}<p/>
+    
+    `
+    monsterCont.appendChild(monsterDiv)
+  }
+  
+  function addForm(){
+    let h1 = document.querySelector("h1")
+    let form = document.createElement('form')
+    form.innerHTML= 
+    `
+    <label for="fname" ></label>
+    <input type="text" id="name" name="name" placeholder="name">
+    <label for="age"></label>
+    <input type="text" id="age" name="age" placeholder="age">
+    <label for="description"></label>
+    <input type="text" id="description" name="description" placeholder="description">
+    <button>create monster</button>
+    `
+    h1.appendChild(form)
+    //  debugger
+    form.addEventListener("submit", function(e){
+      
+      e.preventDefault()
+      
+      let name= e.target.name.value
+      let age= e.target.age.value 
+      let description= e.target.description.value  
+      
+      let newMonster = {
+        name:name, 
+        age:age,
+        description: description
+      }
+      console.log(newMonster)
+      renderMonsters(newMonster)
+      
+      fetch(base_link, {
+        method: "POST" ,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          newMonster
+        })
+      })
+      
+    })//eventListener
+    
+  }//addForm
+  
+  addForm()
+  
+  // let page_url = `http://localhost:3000/monsters/?_limit=50&_page=${pageNumber}`
+  
+  debugger
+  
+  let pageNumber = base_link.charAt(48)
+  let newPageNumberF = pageNumber+1
+  let newPageNumberB = pageNumber-1
+  
+  document.addEventListener("click", function(e){
+    if (e.target.id ==="forward")
+    {
+      let newPageUrlF = `http://localhost:3000/monsters/?_limit=50&_page=${newPageNumberF}`
 
- let base_link = "http://localhost:3000/monsters/?_limit=50&_page=3"
-
- fetch(base_link)
- .then(resp =>resp.json())
- .then(monsters => monsters.forEach(monster =>{
+      fetch(newPageUrlF)
+      .then(resp =>resp.json())
+      .then(monsters => monsters.forEach(monster =>{
      renderMonsters(monster)
      })
- )
+    )
+      
+    } else if (e.target.id==="back"){
+      let newPageUrlB = `http://localhost:3000/monsters/?_limit=50&_page=${newPageNumberB}`
 
-    function renderMonsters(monster){
-        let monsterCont = document.querySelector("#monster-container")
-        let monsterDiv = document.createElement('div')
-        monsterDiv.innerHTML=`
-        <h2>${monster.name}<h2/>
-        <h4>${monster.age}<h4/>
-        <p>${monster.description}<p/>
-        `
-        monsterCont.appendChild(monsterDiv)
-    }
-
-    function addForm(){
-        let h1 = document.querySelector("h1")
-        let form = document.createElement('form')
-        form.innerHTML= 
-        `
-        <label for="fname" ></label>
-        <input type="text" id="name" name="name" placeholder="name">
-        <label for="age"></label>
-        <input type="text" id="age" name="age" placeholder="age">
-        <label for="description"></label>
-        <input type="text" id="description" name="description" placeholder="description">
-        <button>create monster</button>
-        `
-         h1.appendChild(form)
-        //  debugger
-         form.addEventListener("submit", function(e){
-            
-            e.preventDefault()
-            
-
-            let name= e.target.name.value
-            let age= e.target.age.value 
-            let decription= e.target.decription.value  
-
-            let newMonster = {
-                name:name, 
-                age:age,
-                description: description
-            }
-
-            renderMonsters(newMonster)
-            debugger
-
-             fetch(base_link, {
-                method: "POST" ,
-                headers: {
-                  "Content-Type": "application/json",
-                  Accept: "application/json"
-                },
-                body: JSON.stringify({
-                    name: name, 
-                    age: age, 
-                    decription: decription
-                  })
-             })
-             
-         })//eventListener
-          
-    }
-
-  addForm()
+      fetch(newPageUrlB)
+      .then(resp =>resp.json())
+      .then(monsters => monsters.forEach(monster =>{
+     renderMonsters(monster)
+     })
+    )
+  }
+  // debugger
+})
+      
 
     
 })//DOMLoaded
